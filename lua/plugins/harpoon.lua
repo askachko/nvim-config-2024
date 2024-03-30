702509ea424014d4374752bf5da2bc613396da95
Alexander Skachko <alexanderskachko@Alexanders-MacBook-Pro.local>
2024-03-30 13:09:34 -0400

    added harpoon and did some simple configs

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,25 @@
+return {
+  "ThePrimeagen/harpoon",
+  config = function()
+    require("telescope").load_extension("harpoon")
+
+    vim.keymap.set("n", "<leader>fm", ":Telescope harpoon marks<CR>", {})
+
+    local mark = require("harpoon.mark")
+    vim.keymap.set("n", "<leader>mf", mark.add_file, {})
+
+    local ui = require("harpoon.ui")
+    vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, {})
+    vim.keymap.set("n", "<C-k>", ui.nav_prev, {})
+    vim.keymap.set("n", "<C-j>", ui.nav_next, {})
+
+    require("harpoon").setup({
+      global_settings = {
+        save_on_toggle = false,
+        save_on_change = true,
+        mark_branch = true,
+      },
+    })
+
+  end,
+}
