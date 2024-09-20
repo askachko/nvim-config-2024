00e7fcea246bd55696669f96e16dc11f19ddc72f
Alexander Skachko <alexanderskachko@Alexanders-MBP.attlocal.net>
2024-09-20 04:42:55 -0400

    made changes to the tree

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,88 +1,95 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		-- Load Catppuccin theme palette function
-		local catppuccin_theme = function(flavour)
-			local C = require("catppuccin.palettes").get_palette(flavour)
-			local O = require("catppuccin").options
-			local catppuccin = {}
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    -- Load Catppuccin theme palette function
+    local catppuccin_theme = function(flavour)
+      local C = require("catppuccin.palettes").get_palette(flavour)
+      local O = require("catppuccin").options
+      local catppuccin = {}
 
-			local transparent_bg = O.transparent_background and "NONE" or C.mantle
+      local transparent_bg = O.transparent_background and "NONE" or C.mantle
 
-			catppuccin.normal = {
-				a = { bg = C.blue, fg = C.mantle, gui = "bold" },
-				b = { bg = C.surface0, fg = C.blue },
-				c = { bg = transparent_bg, fg = C.text },
-			}
+      catppuccin.normal = {
+        a = { bg = C.blue, fg = C.mantle, gui = "bold" },
+        b = { bg = C.surface0, fg = C.blue },
+        c = { bg = transparent_bg, fg = C.text },
+      }
 
-			catppuccin.insert = {
-				a = { bg = C.green, fg = C.base, gui = "bold" },
-				b = { bg = C.surface0, fg = C.green },
-			}
+      catppuccin.insert = {
+        a = { bg = C.green, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.green },
+      }
 
-			catppuccin.terminal = {
-				a = { bg = C.green, fg = C.base, gui = "bold" },
-				b = { bg = C.surface0, fg = C.green },
-			}
+      catppuccin.terminal = {
+        a = { bg = C.green, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.green },
+      }
 
-			catppuccin.command = {
-				a = { bg = C.peach, fg = C.base, gui = "bold" },
-				b = { bg = C.surface0, fg = C.peach },
-			}
+      catppuccin.command = {
+        a = { bg = C.peach, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.peach },
+      }
 
-			catppuccin.visual = {
-				a = { bg = C.mauve, fg = C.base, gui = "bold" },
-				b = { bg = C.surface0, fg = C.mauve },
-			}
+      catppuccin.visual = {
+        a = { bg = C.mauve, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.mauve },
+      }
 
-			catppuccin.replace = {
-				a = { bg = C.red, fg = C.base, gui = "bold" },
-				b = { bg = C.surface0, fg = C.red },
-			}
+      catppuccin.replace = {
+        a = { bg = C.red, fg = C.base, gui = "bold" },
+        b = { bg = C.surface0, fg = C.red },
+      }
 
-			catppuccin.inactive = {
-				a = { bg = transparent_bg, fg = C.blue },
-				b = { bg = transparent_bg, fg = C.surface1, gui = "bold" },
-				c = { bg = transparent_bg, fg = C.overlay0 },
-			}
+      catppuccin.inactive = {
+        a = { bg = transparent_bg, fg = C.blue },
+        b = { bg = transparent_bg, fg = C.surface1, gui = "bold" },
+        c = { bg = transparent_bg, fg = C.overlay0 },
+      }
 
-			return catppuccin
-		end
+      return catppuccin
+    end
 
-		-- Set the flavour you want
-		local flavour = "macchiato" -- Change to your preferred flavour
+    -- Set the flavour you want
+    local flavour = "macchiato" -- Change to your preferred flavour
 
-		-- Setup lualine with the custom catppuccin theme
-		require("lualine").setup({
-       options = {
-				theme = catppuccin_theme(flavour), -- Use the provided function
-    component_separators = '',
-    section_separators = { left = '', right = '' },
-  },
-  sections = {
-    lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-    lualine_b = { 'filename', 'branch' },
-    lualine_c = {
-      '', --[[ add your center compoentnts here in place of this comment ]]
-    },
-    lualine_x = {},
-    lualine_y = { 'filetype', 'progress' },
-    lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { 'filename' },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { 'location' },
-  },
-  tabline = {},
-  extensions = {},
-		})
-	end,
+    -- Setup lualine with the custom catppuccin theme
+    require("lualine").setup({
+      options = {
+         disabled_filetypes = {
+      statusline = {
+            "NvimTree",
+          },
+          winbar = {
+            "NvimTree",
+          },
+        },
+        theme = catppuccin_theme(flavour), -- Use the provided function
+        component_separators = "",
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_b = { "filename", "branch" },
+        lualine_c = {
+          "", --[[ add your center compoentnts here in place of this comment ]]
+        },
+        lualine_x = {},
+        lualine_y = { "filetype", "progress" },
+        lualine_z = {
+          { "location", separator = { right = "" }, left_padding = 2 },
+        },
+      },
+      inactive_sections = {
+        lualine_a = { "filename" },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { "location" },
+      },
+      tabline = {},
+      extensions = {},
+    })
+  end,
 }
-
---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local tree = require("nvim-tree")
+    tree.setup({
+      view = {
+        width = 25,
+      },
+      renderer = {
+        icons = {
+          git_placement = "signcolumn",
+          show = {
+            file = true,
+            folder = true,
+            folder_arrow = false,
+            git = true,
+          },
+        },
+      },
+    })
+    vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+  end,
+}
